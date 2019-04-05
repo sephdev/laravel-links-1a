f44ee9ee6f748e66c9434ae925a2dcf6fe27c3dc
Joseph Herbert Tandingan <jh.tandingan@coi.local>
2019-04-05 16:53:00 +0800

    Add Submit Form

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,8 @@
 |
 */
 
+use Illuminate\Http\Request;
+
 Route::get('/', function () {
     $links = \App\Link::all();
 
@@ -24,4 +26,16 @@ Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('/submit', function() {
     return view('submit');
+});
+
+Route::post('/submit', function (Request $request) {
+    $data = $request->validate([
+        'title' => 'required|max:255',
+        'url' => 'required|url|max:255',
+        'description' => 'required|max:255',
+    ]);
+
+    $link = tap(new App\Link($data))->save();
+
+    return redirect('/');
 });
